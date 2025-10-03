ca3c24aea29ca625840cfdadcac4751f15771aad
Kirk Wetherill <megwetherill@gmail.com>
2025-10-03 17:53:26 -0500

    update

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,15 @@
+
+// import { Link } from 'react-router-dom';
+// import Button from '../components/Button';
+import './HomeScreen.scss';
+
+function HomeScreen() {
+    return (
+        <div id="home-screen" className="is-inverse">
+            <i className="lds-logo" />
+            {/* <div>Demo</div> */}
+        </div>
+    );
+}
+
+export default HomeScreen;
